bb58c89607d0c9f1ec6e21f70fc86fbb0e539c50
munteanu94 <117288866+munteanu94@users.noreply.github.com>
2023-06-06 23:14:48 +0300

    work for project

--- projectWeb/project.js
+++ projectWeb/project.js
@@ -5,3 +5,18 @@ if(menuIcons) {
         navList.classList.toggle("open");
     })
 }
+
+//popup
+const popup = document.querySelector(".popup");
+const popupClose = document.querySelector(".popupClose");
+
+if(popup) {
+    popupClose.addEventListener("click", () => {
+        popup.classList.add("hidePopap");
+    })
+    window.addEventListener('load', () => {
+        setTimeout(() =>{
+            popup.classList.remove("hidePopap");
+        },1000)
+    })
+}
