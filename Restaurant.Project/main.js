3b7d85209956901fedce0c1243eff4156b4d1434
munteanu94 <117288866+munteanu94@users.noreply.github.com>
2023-06-09 20:57:04 +0300

    work continue

--- Restaurant.Project/main.js
+++ Restaurant.Project/main.js
@@ -7,3 +7,39 @@ document.querySelector(".menu").onclick = () =>{
 document.querySelector(".navList .close").onclick = () =>{
     menu.classList.remove('show');
 }
+// video 
+const containerVideo = document.querySelector(".header .containerVideo");
+const video = document.querySelector('.video');
+const btn = document.querySelector('.controlVideo');
+
+document.querySelector(".watchBtn").onclick = () =>{
+    containerVideo.classList.add('active');
+}
+document.querySelector(".closeVideo").onclick = () =>{
+    containerVideo.classList.remove('active');
+}
+
+function controlVideo (){
+   if(video.paused){
+    btn.innerHTML = "<i class='bx bx-pause'></i>";
+    video.play();
+   }else{
+    btn.innerHTML = "<i class='bx bx-play'></i>";
+    video.pause();
+   }
+}
+btn.addEventListener("click", controlVideo);
+
+
+const login = document.querySelector('.formContainer');
+
+['.loginLink', '.login'].forEach( (e) =>{
+document.querySelector(e).onclick = () =>{
+    login.classList.add('active');
+    menu.classList.remove('show');
+}
+})
+
+document.querySelector('.closeForm').onclick = () =>{
+    login.classList.remove('active');
+}
