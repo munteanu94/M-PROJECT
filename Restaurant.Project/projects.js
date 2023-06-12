5d1597c7d1587f6df6efa7c3ce724ed8c19ed7c3
munteanu94 <117288866+munteanu94@users.noreply.github.com>
2023-06-12 21:54:34 +0300

    work

--- Restaurant.Project/projects.js
+++ Restaurant.Project/projects.js
@@ -14,3 +14,44 @@ const products = new Swiper (".productsSlide", {
         }
     },
 })
+const productsContainer = document.getElementById("productsContainer");
+
+
+const getProducts = async () => {
+    try {
+      const results = await fetch("./products.json");
+      const data = await results.json();
+      const products = data.products;
+      return products;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+console.log(products.json);
+
+  window.addEventListener("DOMContentLoaded", async function () {
+    let products = await getProducts();
+    products = products.filter((product) => product.category === "Burger");
+    displayProducts(products);
+  });
+function displayProducts(items) {
+    let display = items.map((product) =>
+    `<div class="slide">
+    <div class="product">
+        <img src="/Restaurant.Project/imagine/burgher.jpeg">
+        <div class="overlay">
+            <h4>Burger</h4>
+            <div class="flex">
+                <span class="price">$5</span>
+                <span class="rating"><i class="bx bxs-star">5</i></span>
+            </div>
+        </div>
+    </div>
+</div>`
+    )
+    display = display.join("");
+    if (productsContainer) {
+        productsContainer.innerHTML = display;
+    }
+
+}
