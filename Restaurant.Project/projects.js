939a42f3861834c8c2871dd9348856f014148984
munteanu94 <117288866+munteanu94@users.noreply.github.com>
2023-06-18 23:09:49 +0300

    slider

--- Restaurant.Project/projects.js
+++ Restaurant.Project/projects.js
@@ -1,57 +1,37 @@
-const products = new Swiper (".productsSlide", {
+
+/*const swiper = new Swiper(".mySwiper", {
     grabCursor: true,
     slidesPerView: 1,
-    spaceBetween: 30,
-    watchOverflow: true,
-    navigation: {
-        nextEl: '.customNext',
-        prevEl: '.customPrev',
-        lockClass: "no",
+    spaceBetween: 70,
+    pagination: {
+       el: ".swiper-pagination",
+       clickable: true,
     },
     breakpoints: {
-        900: {
+        567: {
         slidesPerView: 2,
-        }
+        },
+        996: {
+            slidesPerView: 3,
+        },
     },
-})
-const productsContainer = document.getElementById("productsContainer");
+});*/
 
-
-const getProducts = async () => {
-    try {
-      const results = await fetch("./products.json");
-      const data = await results.json();
-      const products = data.products;
-      return products;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-console.log(products.json);
-
-  window.addEventListener("DOMContentLoaded", async function () {
-    let products = await getProducts();
-    products = products.filter((product) => product.category === "Burger");
-    displayProducts(products);
+const swiper = new Swiper(".mySwiper", {
+    grabCursor: true,
+    slidesPerView: 1,
+    spaceBetween: 70,
+    pagination: {
+      el: ".custom-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      567: {
+        slidesPerView: 2,
+      },
+      996: {
+        slidesPerView: 3,
+      },
+    },
   });
-function displayProducts(items) {
-    let display = items.map((product) =>
-    `<div class="slide">
-    <div class="product">
-        <img src="/Restaurant.Project/imagine/burgher.jpeg">
-        <div class="overlay">
-            <h4>Burger</h4>
-            <div class="flex">
-                <span class="price">$5</span>
-                <span class="rating"><i class="bx bxs-star">5</i></span>
-            </div>
-        </div>
-    </div>
-</div>`
-    )
-    display = display.join("");
-    if (productsContainer) {
-        productsContainer.innerHTML = display;
-    }
-
-}
+  
