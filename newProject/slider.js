871c516701d23faef9285acf7e9e0d98f54b16fe
munteanu94 <117288866+munteanu94@users.noreply.github.com>
2023-07-02 21:27:34 +0300

    calculator rate

--- newProject/slider.js
+++ newProject/slider.js
@@ -41,3 +41,49 @@ const slideFird = new Swiper(".SwiperFird", {
 
     }
 })
+
+// calculator rate 
+
+const formCalc = document.querySelector(".formCalc");
+const loanAmount = document.getElementById("loanAmount");
+const interestRate = document.getElementById("rate");
+const payMonths = document.getElementById("months");
+const calcBtn = document.querySelector(".calcBtn");
+const clearBtn = document.querySelector(".clearBtn");
+const paymentInfo = document.querySelectorAll(".paymentInfo div span");
+
+formCalc.addEventListener("submit", (e) => {
+    e.preventDefault();
+    showPaymentInfo();
+})
+
+clearBtn.addEventListener("click", clearResult);
+
+function showPaymentInfo() {
+    let monthlyPayment = calcMonthlyPayment(loanAmount.value, interestRate.value,
+    payMonths.value);
+    paymentInfo[0].innerHTML  = `$${loanAmount.value.toLocaleString()}`;
+    paymentInfo[1].innerHTML  = `$${interestRate.value}%`;
+    paymentInfo[2].innerHTML  = payMonths.value;
+    paymentInfo[3].innerHTML  = `$${parseFloat(monthlyPayment).toLocaleString()}`;
+
+}
+let interestRateValue = interestRate.value;
+if(interestRateValue == "" || "0") {
+    interestRate.value = "10";
+    interestRateValue = 10;
+}
+
+function calcMonthlyPayment(PV, i, n) {
+    i = (i / 100) /12;
+    let PMT = (PV * i * Math.pow(1 +i, n)) / (Math.pow(1 + i, n) - 1);
+    return PMT.toFixed(2);
+
+}
+
+function clearResult() {
+    formCalc.reset();
+    paymentInfo.forEach(item => {
+        item.innerHTML = "&mdash;";
+    });
+}
